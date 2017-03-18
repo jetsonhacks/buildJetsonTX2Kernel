51495464ee022df898f627c1efc948b506b61abc
jetsonhacks <jim@jetsonhacks.com>
2017-03-17 22:04:42 -0700

    Initial Commit

--- scripts/makeKernel1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Finishes building the kernel and modules
-# Assumes that the .config file is available
-cd /usr/src/kernel
-make -j4
-make modules
-make modules_install
-
--- scripts/patchKernel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-patch /usr/src/kernel/drivers/platform/tegra/tegra21_clocks.c tegraClocks.patch
-
