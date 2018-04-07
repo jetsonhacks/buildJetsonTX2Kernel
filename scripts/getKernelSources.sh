c60b63f9a13f64d5b78297f7ee8ae22114b5bd99
jetsonhacks <jim@jetsonhacks.com>
2018-04-06 20:46:34 -0700

    Wget only newer

--- scripts/getKernelSources.sh
+++ scripts/getKernelSources.sh
@@ -3,7 +3,7 @@ apt-add-repository universe
 apt-get update
 apt-get install qt5-default pkg-config -y
 cd /usr/src
-wget http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/BSP/source_release.tbz2
+wget -N http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/BSP/source_release.tbz2
 echo 'Extracting kernel_src.tbz2 from source release'
 tar -xvf source_release.tbz2 public_release/kernel_src.tbz2
 echo 'Expanding kernel_src.tbz2'
