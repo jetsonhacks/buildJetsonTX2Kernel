1d386add4934731811ee9e4ca5d320cf0cf06a26
jetsonhacks <jim@jetsonhacks.com>
2018-04-20 19:56:26 -0700

    Correct kernel source address

--- scripts/getKernelSources.sh
+++ scripts/getKernelSources.sh
@@ -3,13 +3,13 @@ apt-add-repository universe
 apt-get update
 apt-get install qt5-default pkg-config -y
 cd /usr/src
-wget -N http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/BSP/source_release.tbz2
-echo 'Extracting kernel_src.tbz2 from source release'
-tar -xvf source_release.tbz2 public_release/kernel_src.tbz2
-echo 'Expanding kernel_src.tbz2'
+wget -N http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/GA/BSP/tx2_sources.tbz2
+sudo tar -xvf tx2_sources.tbz2 public_release/kernel_src.tbz2
 tar -xvf public_release/kernel_src.tbz2
+# Space is tight; get rid of the compressed kernel source
 rm -r public_release
 cd kernel/kernel-4.4
+# Go get the default config file; this becomes the new system configuration
 zcat /proc/config.gz > .config
 # Ready to configure kernel
 make xconfig
