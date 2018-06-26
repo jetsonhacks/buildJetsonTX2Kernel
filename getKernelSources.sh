86d207af7e02e3c6b4b227545428a08a97462ab4
Shreeyak <ss.shrek7@gmail.com>
2018-06-26 14:23:57 +0530

    Updated repo to build kernel for 28.2.1

--- getKernelSources.sh
+++ getKernelSources.sh
@@ -7,7 +7,7 @@ echo "$JETSON_DESCRIPTION"
 echo "Jetpack $JETSON_JETPACK [L4T $JETSON_L4T]"
 
 # Check to make sure we're installing the correct kernel sources
-L4TTarget="28.2"
+L4TTarget="28.2.1"
 if [ $JETSON_L4T == $L4TTarget ] ; then
    echo "Getting kernel sources"
    sudo ./scripts/getKernelSources.sh
@@ -17,7 +17,7 @@ else
    echo "==== L4T Kernel Version Mismatch! ============="
    tput sgr0
    echo ""
-   echo "This repository branch is for installing the kernel sources for L4T "$L4TTarget 
+   echo "This repository branch is for installing the kernel sources for L4T "$L4TTarget
    echo "You are attempting to use these kernel sources on a L4T "$JETSON_L4T "system."
    echo "The kernel sources do not match their L4T release!"
    echo ""
@@ -27,7 +27,7 @@ else
    echo "$ git tag -l"
    echo "And then checkout the latest version: "
    echo "For example"
-   echo "$ git checkout v1.0-L4T"$JETSON_L4T
+   echo "$ git checkout L4T"$JETSON_L4T
    echo ""
 fi
 
--- scripts/jetson_variables.sh
+++ scripts/jetson_variables.sh
@@ -6,29 +6,29 @@
 # modification, are permitted provided that the following conditions
 # are met:
 #
-# 1. Redistributions of source code must retain the above copyright 
+# 1. Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the copyright holder nor the names of its 
-#    contributors may be used to endorse or promote products derived 
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived
 #    from this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
 # FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# NVIDIA Identify version 
-# reference: 
+# NVIDIA Identify version
+# reference:
 # https://devtalk.nvidia.com/default/topic/1014424/jetson-tx2/identifying-tx1-and-tx2-at-runtime/
 # https://devtalk.nvidia.com/default/topic/996988/jetson-tk1/chip-uid/post/5100481/#5100481
 
@@ -53,62 +53,66 @@ if [ -f /etc/nv_tegra_release ]; then
     JETSON_L4T_STRING=$(head -n 1 /etc/nv_tegra_release)
 
     # Load release and revision
-    JETSON_L4T_RELEASE=$(echo $JETSON_L4T_STRING | cut -f 1 -d ',' | sed 's/\# R//g' | cut -d ' ' -f1)
-    JETSON_L4T_REVISION=$(echo $JETSON_L4T_STRING | cut -f 2 -d ',' | sed 's/\ REVISION: //g' | cut -d. -f1)
+    JETSON_L4T_RELEASE=$(echo $JETSON_L4T_STRING | cut -f 1 -d ',' | sed 's,\# R,,' | cut -d ' ' -f 1)
+    JETSON_L4T_REVISION=$(echo $JETSON_L4T_STRING | cut -f 2 -d ',' | sed 's,\ REVISION: ,,g' | cut -d. -f 1)
     # unset variable
     unset JETSON_L4T_STRING
-    
+
     # Write Jetson description
     JETSON_L4T="$JETSON_L4T_RELEASE.$JETSON_L4T_REVISION"
 
     # Write version of jetpack installed
     # https://developer.nvidia.com/embedded/jetpack-archive
-    if [ "$JETSON_BOARD" = "TX2i" ] ; then 
+    if [ "$JETSON_BOARD" = "TX2i" ] ; then
         case $JETSON_L4T in
-            "28.2") 
+            "28.2.1")
+                    JETSON_JETPACK="3.2.1" ;;
+            "28.2")
                JETSON_JETPACK="3.2" ;;
             *)
                JETSON_JETPACK="UNKNOWN" ;;
-        esac        
+        esac
     elif [ "$JETSON_BOARD" = "TX2" ] ; then
         case $JETSON_L4T in
-            "28.2") 
+            "28.2.1")
+                    JETSON_JETPACK="3.2.1" ;;
+            "28.2")
                     JETSON_JETPACK="3.2" ;;
-            "28.1") 
+            "28.1")
                     JETSON_JETPACK="3.1" ;;
-            "27.1") 
+            "27.1")
                     JETSON_JETPACK="3.0" ;;
             *)
                JETSON_JETPACK="UNKNOWN" ;;
         esac
     elif [ "$JETSON_BOARD" = "TX1" ] ; then
         case $JETSON_L4T in
-            "28.2") 
+            "28.2")
                     JETSON_JETPACK="3.2" ;;
-            "28.1") 
+            "28.1")
                     JETSON_JETPACK="3.1" ;;
-            "24.2.1") 
+            "24.2.1")
                     JETSON_JETPACK="3.0 or 2.3.1" ;;
-            "24.2") 
+            "24.2")
                     JETSON_JETPACK="2.3" ;;
-            "24.1") 
+            "24.1")
                     JETSON_JETPACK="2.2.1 or 2.2" ;;
-            "23.2") 
+            "23.2")
                     JETSON_JETPACK="2.1" ;;
-            "23.1") 
+            "23.1")
                     JETSON_JETPACK="2.0" ;;
             *)
                JETSON_JETPACK="UNKNOWN" ;;
         esac
     elif [ "$JETSON_BOARD" ="TK1" ] ; then
         case $JETSON_L4T in
-            "21.5") 
+            "21.5")
                     JETSON_JETPACK="2.3.1 or 2.3" ;;
-            "21.4") 
+            "21.4")
                     JETSON_JETPACK="2.2 or 2.1 or 2.0 or DP 1.2" ;;
-            "21.3") 
+            "21.3")
                     JETSON_JETPACK="DP 1.1" ;;
-            "21.2") 
+            "21.2")
                     JETSON_JETPACK="DP 1.0" ;;
             *)
                JETSON_JETPACK="UNKNOWN" ;;
