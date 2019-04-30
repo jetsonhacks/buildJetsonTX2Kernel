5b87cc00f2f8ee3bad3ddf380d532f3ad0f4065b
Alexander Rashed <alexander.rashed@gmail.com>
2019-04-30 17:24:04 +0200

    Fix kernel source cleanup

--- scripts/getKernelSourcesNoGUI.sh
+++ scripts/getKernelSourcesNoGUI.sh
@@ -7,7 +7,7 @@ wget -N https://developer.download.nvidia.com/embedded/L4T/r32_Release_v1.0/jax-
 sudo tar -xvf JAX-TX2-public_sources.tbz2 public_sources/kernel_src.tbz2
 tar -xvf public_sources/kernel_src.tbz2
 # Space is tight; get rid of the compressed kernel source
-rm -r public_release
+rm -r public_sources
 cd kernel/kernel-4.9
 # Go get the default config file; this becomes the new system configuration
 zcat /proc/config.gz > .config
