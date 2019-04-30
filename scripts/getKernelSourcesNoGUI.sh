3e4047bb52cea96c90eb69440d19e8d84e28536f
Alexander Rashed <alexander.rashed@gmail.com>
2019-04-30 11:40:23 +0200

    Fix archive name

--- scripts/getKernelSourcesNoGUI.sh
+++ scripts/getKernelSourcesNoGUI.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install pkg-config -y
 cd /usr/src
 wget -N https://developer.download.nvidia.com/embedded/L4T/r32_Release_v1.0/jax-tx2/BSP/JAX-TX2-public_sources.tbz2
-sudo tar -xvf public_sources.tbz2 public_release/kernel_src.tbz2
+sudo tar -xvf JAX-TX2-public_sources.tbz2 public_release/kernel_src.tbz2
 tar -xvf public_release/kernel_src.tbz2
 # Space is tight; get rid of the compressed kernel source
 rm -r public_release
